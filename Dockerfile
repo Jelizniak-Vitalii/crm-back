bd9927a03d98fe322e9705eab956ae6789090d8c
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2024-03-23 13:17:38 +0200

    change port

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ COPY package*.json ./
 COPY . .
 RUN npm install
 RUN npm run build
-EXPOSE 4300
+EXPOSE 3000
 CMD [ "node", "dist/index.js" ]
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker stop crm-back
 docker rm --force crm-back
 docker rmi crm-back-image
 docker build . -t crm-back-image
-docker run -d -p 4300:4300 --name crm-back crm-back-image
+docker run -d -p 3000:3000 --name crm-back crm-back-image
