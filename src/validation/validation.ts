570eff7b01ec2b54ca26cb84ca2897babc2644d8
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2024-03-23 13:41:06 +0200

    fix

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -2,16 +2,12 @@ import Joi from 'joi';
 import { CustomError, HttpStatus } from '../core/errors';
 
 export async function validate<T>(data: T, schema: Joi.ObjectSchema) {
-  try {
-    const { error }: Joi.ValidationResult = schema.validate(data);
+  const { error }: Joi.ValidationResult = schema.validate(data);
 
-    if (error) {
-      throw new CustomError(error.details[0].message, HttpStatus.BAD_REQUEST);
-    }
-
-    // @ts-ignore
-    return error ? error.details[0].message : null;
-  } catch (error) {
-    throw new CustomError('Ошибка при валидации данных');
+  if (error) {
+    throw new CustomError(error.details[0].message, HttpStatus.BAD_REQUEST);
   }
+
+  // @ts-ignore
+  return error ? error.details[0].message : null;
 }
